81e19c569f97af248761b6040201be3b99afcf4d
JosepAntoni <costacamps8@gmail.com>
2019-03-23 19:44:45 +0100

    Video updated

--- public/video.js
+++ public/video.js
@@ -14,24 +14,40 @@ var score = 0;
 		navigator.mozGetUserMedia ||
 		navigator.msGetUserMedia);
 	
-	navigator.getMedia(
-		{
-			video: true,
-			audio: false
-		},
-		function(stream) {
-			if (navigator.mozGetUserMedia) {
-				video.mozSrcObject = stream;
-			} else {
-				var vendorURL = window.URL || window.webkitURL;
-				video.srcObject = stream;
-			}
-			video.play();
-		},
-		function(err) {
-			console.log("An error occured! " + err);
+	async function loadVideo() {
+		const video = await setupCamera();
+		video.play();
+		
+		return video;
+	}
+	
+	async function setupCamera() {
+		if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+			throw new Error(
+				'Browser API navigator.mediaDevices.getUserMedia not available');
 		}
-	);
+		
+		const video = document.getElementById('video');
+		video.width = videoWidth;
+		video.height = videoHeight;
+		
+		const mobile = isMobile();
+		const stream = await navigator.mediaDevices.getUserMedia({
+			'audio': false,
+			'video': {
+				facingMode: 'user',
+				width: mobile ? undefined : videoWidth,
+				height: mobile ? undefined : videoHeight,
+			},
+		});
+		video.srcObject = stream;
+		
+		return new Promise((resolve) => {
+			video.onloadedmetadata = () => {
+				resolve(video);
+			};
+		});
+	}
 	
 	video.addEventListener('canplay', function(ev){
 		if (!streaming) {
@@ -74,9 +90,7 @@ var score = 0;
 	}
 
 	function compareTwoPoses(pose_user, pose_default) {
-		var i;
 		arms(pose_user, pose_default);
-		console.log(pose_user.keypoints);
 	}
 
 	function arms(pose_user, pose_default) {
@@ -131,6 +145,25 @@ var score = 0;
 			console.log("Default x: " + pose_default.position.x);
 			console.log("Default y: " + pose_default.position.y);
 		} 
+	}
+	
+	export async function bindPage() {
+		// Load the PoseNet model weights with architecture 0.75
+		const net = await posenet.load(0.75);
+		
+		let video;
+		
+		try {
+			video = await loadVideo();
+		} catch (e) {
+			let info = document.getElementById('info');
+			info.textContent = 'this browser does not support video capture,' +
+				'or this device does not have a camera';
+			info.style.display = 'block';
+			throw e;
+		}
+		
+		detectPoseInRealTime(video, net);
 	}
 	
 	startbutton.addEventListener('click', function(ev){
