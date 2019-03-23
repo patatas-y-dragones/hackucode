48b45d03c2ae9d2ec6754a1822a748a236487ed5
JosepAntoni <costacamps8@gmail.com>
2019-03-23 22:01:41 +0100

    Code refactor

--- public/video.js
+++ public/video.js
@@ -1,78 +1,47 @@
-var score = 0;
-	
-	var streaming = false,
-		video        = document.querySelector('#video'),
-		canvas       = document.querySelector('#canvas'),
-		photo        = document.querySelector('#photo'),
-		startbutton  = document.querySelector('#startbutton'),
-		width = 320,
-		height = 0;
-	
-	navigator.getMedia = ( navigator.getUserMedia ||
-		navigator.webkitGetUserMedia ||
-		navigator.mozGetUserMedia ||
-		navigator.msGetUserMedia);
-	
-	async function loadVideo() {
-		const video = await setupCamera();
-		video.play();
-		
-		return video;
-	}
-	
-	async function setupCamera() {
-		if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-			throw new Error(
-				'Browser API navigator.mediaDevices.getUserMedia not available');
-		}
-		
-		const video = document.getElementById('video');
-		
-		const stream = await navigator.mediaDevices.getUserMedia({
-			'audio': false,
-			'video': {
-				facingMode: 'user',
-			},
-		});
+var streaming = false,
+video        = document.querySelector('#video'),
+canvas       = document.querySelector('#canvas'),
+photo        = document.querySelector('#photo'),
+startbutton  = document.querySelector('#startbutton'),
+width = 320,
+height = 0;
+
+navigator.getMedia = ( navigator.getUserMedia ||
+navigator.webkitGetUserMedia ||
+navigator.mozGetUserMedia ||
+navigator.msGetUserMedia);
+
+navigator.getMedia(
+{
+	video: true,
+	audio: false
+},
+function(stream) {
+	if (navigator.mozGetUserMedia) {
+		video.mozSrcObject = stream;
+	} else {
+		var vendorURL = window.URL || window.webkitURL;
 		video.srcObject = stream;
-		
-		return new Promise((resolve) => {
-			video.onloadedmetadata = () => {
-				resolve(video);
-			};
-		});
 	}
-	
-	video.addEventListener('canplay', function(ev){
-		if (!streaming) {
-			height = video.videoHeight / (video.videoWidth/width);
-			canvas.setAttribute('width', width);
-			canvas.setAttribute('height', height);
-			streaming = true;
-		}
-	}, false);
-	
-	async function bindPage() {
-		// Load the PoseNet model weights with architecture 0.75
-		const net = await posenet.load(0.75);
-		
-		let video;
-		
-		try {
-			video = await loadVideo();
-		} catch (e) {
-			/*let info = document.getElementById('info');
-			info.textContent = 'this browser does not support video capture,' +
-				'or this device does not have a camera';
-			info.style.display = 'block';*/
-			throw e;
-		}
-		
-		//detectPoseInRealTime(video, net);
-	}
-	
-	startbutton.addEventListener('click', function(ev){
-		takepicture();
-		ev.preventDefault();
-	}, false);
-	bindPage();
+	video.play();
+},
+function(err) {
+	console.log("An error occured! " + err);
+}
+);
+
+video.addEventListener('canplay', function(ev){
+if (!streaming) {
+	height = video.videoHeight / (video.videoWidth/width);
+	video.setAttribute('width', width);
+	video.setAttribute('height', height);
+	canvas.setAttribute('width', width);
+	canvas.setAttribute('height', height);
+	streaming = true;
+}
+}, false);
+
+startbutton.addEventListener('click', function(ev){
+takepicture();
+ev.preventDefault();
+}, false);
