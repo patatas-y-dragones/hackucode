b50b1defb36f69e5022d6500d91a234210bff84f
Oriol <oriolalascercos@gmail.com>
2019-03-23 13:04:58 +0100

    Compared parts of the body

--- public/video.js
+++ public/video.js
@@ -44,33 +44,31 @@ var posep;
 		}
 	}, false);
 	
-	function takepicture() {
+	async function takepicture() {
 		canvas.width = width;
 		canvas.height = height;
 		canvas.getContext('2d').drawImage(video, 0, 0, width, height);
 		var data = canvas.toDataURL('image/png');
 		var photo = document.getElementById('photo');
 		photo.setAttribute('src', data);
-		var pose_user = getData(photo);
-		haveAllTwoPositions(pose_user);
+		haveAllTwoPositions(photo);
 	}
 	
-	function getData(image) {
+	function getData(image, pose_default) {
 		posenet.load().then(async function (net) {
 			return await net.estimateSinglePose(image, imageScaleFactor, flipHorizontal, outputStride)
-		}).then(function (pose) {
-			console.log("GETDATA: " + pose);
+		}).then(function (pose_user) {
+			console.log(pose_user, pose_default);
+			compareTwoPoses(pose_user, pose_default);
 		})
 	}
 
-	function haveAllTwoPositions(pose_user) {
+	async function haveAllTwoPositions(pose_user) {
 		var imgDeft = document.getElementById('img1');
-		console.log("HERE: " + pose_user);
-		posenet.load().then(function (net) {
-			net.estimateSinglePose(imgDeft, imageScaleFactor, flipHorizontal, outputStride)
+		posenet.load().then(async function (net) {
+			return await net.estimateSinglePose(imgDeft, imageScaleFactor, flipHorizontal, outputStride)
 		}).then(function(pose2){
-			console.log(getData(pose_user), pose2);
-			compareTwoPoses(getData(pose_user), pose2);
+			getData(pose_user, pose2)
 		})
 	}
 
@@ -79,13 +77,13 @@ var posep;
 		for(i = 0; i < 17; i++) {
 			comparePartOfBody(pose_user.keypoints[i],pose_default.keypoints[i])
 		}
-		console.log(poser_user.keypoints);
+		console.log(pose_user.keypoints);
 	}
 	
 	function comparePartOfBody(pose_user, pose_default){
-		if (pose_default > 0.75){
-			if (pose_default - pose_user < 0.05){
-				console.log("Correcte");
+		if (pose_default.score > 0.75 && pose_user.score > 0.75){
+			if (pose_default.score - pose_user.score < 0.05){
+				console.log(pose_user);
 			}
 		} 
 	}
